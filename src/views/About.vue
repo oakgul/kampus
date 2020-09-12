c6390d9c1e904d1e0aeff0dba9c977597bf2922e
oakgul <orhan.akgull@gmail.com>
2020-09-12 23:20:16 +0300

    about cleaning

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,5 @@
 <template>
     <div class="about">
-        <h1>Hakkımda Sayfası</h1>
-        <h3> Nam-ı diğer fişşeek yazılımcı yani bendeniz Orhan Akgül hayvan sever, gezgin ve yazılımcı bir vatandaş.
-          Dünyayı gezme hayaliyle freelance dünyasına giriş yapar yapmaz ilham alarak bu kanalı açmış bulunmaktayım.
-          Yaklaşık 11 yaşında ortaokul hocam tarafından yazılıma yöneltilmiş meraklı bir Yüksek Bilgisayar
-          Mühendisidir. Şimdi bakınca 16 yıl olmuş vay be.. Eğer bu sayfayı ziyaret ediyorsanız; ne yapar ne ederim diye
-          merak etmişsiniz demektir 🙂</h3>
-
-        <h3> Yazılım hayatıma C# , C++, C ile başlayıp daha sonra JavaScript ile devam ettim. Lise 3
-          de Borland Delphi 7 görerek artık resmen bu yola baş koyacağımı anlamıştım. Elektronik de bildiğimden dolayı
-          Mikroişlemcilere de kod yazmaktan geri kalmadım elbette 🙂 Delphi 7 ile yaklaşık 8 yıl uğraştım ve hiç
-          bırakmadım diyebilirim. Bu süre zarfında Restoran Otomasyonları, Muhasebe Uygulamaları, Öğrenci Otomasyonları,
-          Dershane Yönetim Sistemleri, Personel Devam Kontrol Sistemleri(PDKS), Fabrika Otomasyonları, Veri Analiz </h3>
-        
-        <blockquote>Herkes bildiğini paylaşırsa, herkes daha fazla öğrenecektir.</blockquote>
+       hakkımızda
     </div>
 </template>
