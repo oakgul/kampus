a2417f046ff4962d54980af900a2a1c8df99a8e5
oakgul <orhan.akgull@gmail.com>
2020-09-19 00:38:34 +0300

    Profile sidebar refactored

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,7 @@
     <div class="profile">
 
         <img class="dashboard-image" src="@/assets/orhanprofil.jpg" alt="profile">
-        <div class="name">Orhan Akgül</div>    
+        <div class="user-name">Orhan Akgül</div>    
         <div class="department">Bilgisayar Mühendisliği</div>
 
         <div class="sidebar-buttons">
@@ -17,93 +17,70 @@
             <img class="sidebar-icons" src="@/assets/help-circle.svg" alt="announce">
             <span>SORU SOR</span>
         </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
 
-    .profile {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        // max-width: 200px;   // daha sonra kaldır!!!
-        min-width: 345px;
-        height: 100vh;
-        background: #F5F5FF;
-        border-top-left-radius: 56px;
-
-        .settings {
-            position: relative;
-        }
-
-        .notification {
-            width: 10px;
-            height: 10px;
-            background-color: orangered;
-            border: 2px solid #F5F5FF;;
-            border-radius: 50%;
-            position: absolute;
-            top: 5px;
-            right: -17px;
-        }
-
-        .dashboard-image {
-    width: 150px;
-    height: 150px;
-}
-
-.name {
+.profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 350px;
+    height: calc(100vh - 66px);
+    background: #F5F5FF;
+    border-top-left-radius: 56px;
     font-family: Roboto;
-    font-size: 20px;
-    margin-top: 10px;
-}
 
-.department {
-    margin-top: 5px;
-    margin-bottom: 150px;
-    font-family: Roboto;
-}
 
-.settings-icon {
-    width: 32px;
-    height: 32px;
-    margin-left: 110px;
-    margin-right: -80px;
-    margin-bottom: 20px;
-    cursor: pointer;
-}
-
-.sidebar-buttons {
-    width: 260px;
-    height: 80px;
-    background-color: #E3E3FC;
-    text-align: center;
-    line-height: 80px;
-    margin-bottom: 20px;
-    border-radius: 40px;
-    position: relative;
-    font-family: Roboto;
-    font-size: 18px;
-    cursor: pointer;
-    transition-duration: 500ms;
-    border: 1px solid white;
-}
-
-.sidebar-buttons:hover {
-    background: white;
-    color: gray;
-    filter: drop-shadow(0px 10px 15px rgba(0, 0, 0, 0.2));
-    margin-left: 20px;
-    border: 1px dashed gray;
-}
-
-.sidebar-icons {
-    width: 42px;
-    height: 42px;
-    position: absolute;
-    left: 20px;
-    top: 20px;
-}
+    .dashboard-image {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        margin-top: 40px;
     }
+
+    .user-name {
+        font-size: 20px;
+        margin-top: 10px;
+    }
+
+    .department {
+        margin-top: 5px;
+        margin-bottom: 150px;
+    }
+
+    .sidebar-buttons {
+        width: 260px;
+        height: 80px;
+        background-color: #E3E3FC;
+        text-align: center;
+        line-height: 80px;
+        margin-bottom: 20px;
+        border-radius: 40px;
+        position: relative;
+        font-size: 18px;
+        cursor: pointer;
+        transition: 500ms all;
+        border: 1px solid white;
+
+        &:hover {
+            background: white;
+            color: gray;
+            filter: drop-shadow(0px 10px 15px rgba(0, 0, 0, 0.2));
+            margin-left: 10px;
+            border: 1px dashed gray;
+        }
+
+        .sidebar-icons {
+            width: 42px;
+            height: 42px;
+            position: absolute;
+            left: 20px;
+            top: 20px;
+        }
+    }
+}
 
 </style>
