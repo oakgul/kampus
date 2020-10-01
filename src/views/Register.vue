db15abacef06d2fdc2110cc028a5c92ad93044c1
oakgul <orhan.akgull@gmail.com>
2020-10-02 00:20:25 +0300

    Register Page refactored

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,34 +2,31 @@
     <div>
         <Header />
         <div class="register">
-        <div class="image">
-            <img src="@/assets/register.svg" alt="register">
-        </div>
-        <div class="register-form">
-            <div>
-                <input v-model="user.name" required type="text" placeholder="Ad"><br>
-                <input v-model="user.surname" required type="text" placeholder="Soyad"><br>
-                <input v-model="user.email" required type="email" placeholder="E-posta"><br>
-                <input v-model="user.password" required type="password" placeholder="Parola"><br>
-                <select v-model="user.gender">
-                    <option value="kadın">Kadın</option>
-                    <option value="erkek">Erkek</option>
-                    <option value="diğer">Diğer</option>
-                </select><br>
-                <select v-model="user.department">
-                    <option selected value="bilgisayar">Bilgisayar Mühendisliği</option>
-                    <option value="kimya">Kimya Mühendisliği</option>
-                    <option value="tab">Biyoloji Mühendisliği</option>
-                </select><br>
-                <select v-model="user.role">
-                    <option value="student">Öğrenci</option>
-                    <option value="admin">Akademisyen</option>
-                </select><br>
+            <div class="register-form">
+                <div>
+                    <input v-model="user.name" required type="text" placeholder="Ad"><br>
+                    <input v-model="user.surname" required type="text" placeholder="Soyad"><br>
+                    <input v-model="user.email" required type="email" placeholder="E-posta"><br>
+                    <input v-model="user.password" required type="password" placeholder="Parola"><br>
+                    <select v-model="user.gender">
+                        <option value="kadın">Kadın</option>
+                        <option value="erkek">Erkek</option>
+                        <option value="diğer">Diğer</option>
+                    </select><br>
+                    <select v-model="user.department">
+                        <option selected value="bilgisayar">Bilgisayar Mühendisliği</option>
+                        <option value="kimya">Kimya Mühendisliği</option>
+                        <option value="tab">Biyoloji Mühendisliği</option>
+                    </select><br>
+                    <select v-model="user.role">
+                        <option value="student">Öğrenci</option>
+                        <option value="admin">Akademisyen</option>
+                    </select><br>
 
-                <button @click="userRegister">Kayıt Ol</button>
+                    <button @click="userRegister">Kayıt Ol</button>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </template>
 
@@ -64,46 +61,51 @@ export default {
 }
 </script>
 
-<style scoped>
-
-    .register {
-        display: flex;
-        margin-top: 50px;
-    }
-
-    input,select{
-        width: 450px;
-        height: 50px;
-        border: 1px solid gray;
-        border-radius: 8px;;
-        margin-bottom: 10px;
-        text-indent: 10px;
-    }
-
-    .cinsiyet {
-        width: 20px;
-        height: 20px;
-        margin: 5px 30px;
-    }
+<style lang="scss" scoped>
+.register {
+    display: flex;
 
     .register-form {
-        margin: auto;
-    }
+        width: 400px;
+        height: 560px;
+        display: flex;
+        flex-direction: column;
+        filter: drop-shadow(0px 10px 15px rgba(0, 0, 0, 0.2));
+        border-radius: 6px;
+        align-items: center;
+        background: white;
+        margin: 70px auto;    
+        padding-top: 15px;
+        
+        input,select{
+            width: 370px;
+            height: 50px;
+            border: 1px solid lightgrey;
+            border-radius: 6px;
+            font-size: 18px;
+            text-indent: 15px;
+            margin-bottom: 15px; 
 
-    button {
-        width: 450px;
-        height: 70px;
-        font-family: Roboto;
-        font-size: 20px;
-        border-radius: 18px;
-        cursor: pointer;
-        background-color: #DAF7A6;
-        margin: auto;
-    }
+            &:focus {
+                border-color:#1877f2;
+            }
+        }
 
-    button:hover {
-        background-color: orange;
-        color: white;
-    }
+        button {
+            width: 370px;
+            height: 50px;
+            color: white;
+            background: #31B057; 
+            font-size: 20px;
+            border-radius: 6px;
+            cursor: pointer;
+            font-weight: bold;
+
+            &:hover {
+                background:  #1877f2;
+            }
+        }
+    }    
+}  
 
 </style>
